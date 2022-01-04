47e19a2e858af89529e35d9dd9208eeadbb493ee
Mati02K <mati02official@gmail.com>
2022-01-04 14:53:05 +0530

    PlayFairCipher Issues have been recrified and been feeded with input.

--- src/PlayFairCipher.java
+++ src/PlayFairCipher.java
@@ -1,32 +1,37 @@
+import java.util.Scanner;
+
+//https://www.youtube.com/watch?v=whEJfas9MAI
 
 public class PlayFairCipher {
-    private String KeyWord        = new String();
-    private String Key            = new String();
-    private char   matrix_arr[][] = new char[5][5];
+    private static String KeyWord        = "";
+    private static String Key            = "";
+    private static final char[][]  matrix_arr = new char[5][5];
 
-    public void setKey(String k)
+//    We are just removing the redundant characters to form a key (for ex if key is eeaab it will become eab)
+    static void setKey(String k)
     {
-        String K_adjust = new String();
+        k = k.toLowerCase();
+        String K_adjust = "";
         boolean flag = false;
         K_adjust = K_adjust + k.charAt(0);
         for (int i = 1; i < k.length(); i++)
         {
             for (int j = 0; j < K_adjust.length(); j++)
             {
-                if (k.charAt(i) == K_adjust.charAt(j))
-                {
+                if (k.charAt(i) == K_adjust.charAt(j)) {
                     flag = true;
+                    break;
                 }
             }
-            if (flag == false)
+            if (!flag)
                 K_adjust = K_adjust + k.charAt(i);
             flag = false;
         }
         KeyWord = K_adjust;
-        System.out.println("Keyword" + KeyWord);
+        System.out.println("Your Keyword : " + KeyWord);
     }
 
-    public void KeyGen()
+    static void KeyGen()
     {
         boolean flag = true;
         char current;
@@ -48,11 +53,11 @@ public class PlayFairCipher {
                 Key = Key + current;
             flag = true;
         }
-        System.out.println(Key);
+        System.out.println("The Codec Matrix :");
         matrix();
     }
-
-    private void matrix()
+//Forming a matrix with the key
+    static void matrix()
     {
         int counter = 0;
         for (int i = 0; i < 5; i++)
@@ -67,11 +72,11 @@ public class PlayFairCipher {
         }
     }
 
-    private String format(String plain_text)
+    static String format(String plain_text)
     {
         int i = 0;
         int len = 0;
-        String text = new String();
+        String text = "";
         len = plain_text.length();
         for (int tmp = 0; tmp < len; tmp++)
         {
@@ -93,7 +98,8 @@ public class PlayFairCipher {
         return text;
     }
 
-    private String[] Divid2Pairs(String new_string)
+//    Split the string into two pairs (for forming Diagram)
+    static String[] Divid2Pairs(String new_string)
     {
         String Original = format(new_string);
         int size = Original.length();
@@ -102,7 +108,7 @@ public class PlayFairCipher {
             size++;
             Original = Original + 'x';
         }
-        String x[] = new String[size / 2];
+        String[] x = new String[size / 2];
         int counter = 0;
         for (int i = 0; i < size / 2; i++)
         {
@@ -112,7 +118,8 @@ public class PlayFairCipher {
         return x;
     }
 
-    public int[] GetDiminsions(char letter)
+//    Getting the position of the character in the char array
+    static int[] GetDiminsions(char letter)
     {
         int[] key = new int[2];
         if (letter == 'j')
@@ -132,20 +139,22 @@ public class PlayFairCipher {
         return key;
     }
 
-    public String encryptMessage(String Source)
+    static String encryptMessage(String Source)
     {
-        String src_arr[] = Divid2Pairs(Source);
-        String Code = new String();
+        Source = Source.toLowerCase();
+        String[] src_arr = Divid2Pairs(Source);
+        String Code = "";
         char one;
         char two;
-        int part1[] = new int[2];
-        int part2[] = new int[2];
-        for (int i = 0; i < src_arr.length; i++)
+        int[] part1 = new int[2];
+        int[] part2 = new int[2];
+        for(int i = 0; i < src_arr.length; i++)
         {
             one = src_arr[i].charAt(0);
             two = src_arr[i].charAt(1);
             part1 = GetDiminsions(one);
             part2 = GetDiminsions(two);
+//            Same Row
             if (part1[0] == part2[0])
             {
                 if (part1[1] < 4)
@@ -157,6 +166,7 @@ public class PlayFairCipher {
                 else
                     part2[1] = 0;
             }
+//            Same Column
             else if (part1[1] == part2[1])
             {
                 if (part1[0] < 4)
@@ -168,6 +178,7 @@ public class PlayFairCipher {
                 else
                     part2[0] = 0;
             }
+//            Rectangle Last case Swap the Columns
             else
             {
                 int temp = part1[1];
@@ -182,19 +193,15 @@ public class PlayFairCipher {
 
     public static void main(String[] args)
     {
-        PlayFairCipher x = new PlayFairCipher();
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter a keyword:");
-//        String keyword = sc.next();
-        String keyword = "Monarchy";
-        x.setKey(keyword);
-        x.KeyGen();
-        String key_input = "instruments";
-//        System.out.println("Enter word to encrypt:");
-//        String key_input = sc.next();
-        System.out.println("Encryption: " + x.encryptMessage(key_input));
-        System.out.println('z'-'a');
-//
-//        sc.close();
+        System.out.println("Welcome to PlayFair Cipher");
+        System.out.println("Enter the Keyword");
+        Scanner sc = new Scanner(System.in);
+        String keyword = sc.nextLine();
+        setKey(keyword);
+        KeyGen();
+        System.out.println("Enter the plaintext to be encrypted");
+        String key_input = sc.nextLine();
+        System.out.println("Your Encrypted Message: " + encryptMessage(key_input));
+
     }
 }
